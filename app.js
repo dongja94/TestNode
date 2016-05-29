ffbe731012fe960ef4baef14ecbdbcbda7d83cb0
dongja94 <dongja94@gmail.com>
2016-05-29 16:32:07 +0900

    update user

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.limit('10mb'));
+app.use(express.bodyParser({uploadDir: __dirname + '/tmp'}));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,8 +31,14 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/profile',user.profile);
+app.get('/modify',user.modify);
+app.post('/user/new',user.newuser);
 app.post('/test', test.testarray);
+app.post('/user/update',user.updateuser);
+app.post('/user/photo',user.photo);
 app.get('/:user/friend',user.friend);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/user.js
+++ routes/user.js
@@ -17,3 +17,40 @@ exports.friend = function(req, res) {
   }
   res.json(friendlist);
 };
+
+exports.profile = function(req, res) {
+	var user = {};
+	console.log(req.query);
+	user.name = "user name " + req.query.user;
+	user.email = "user email";
+	res.json(user);
+}
+
+exports.modify = function(req, res) {
+	var user = {};
+	user.name = "name " + req.query.name;
+	user.email = "email " + req.query.email;
+	res.json(user);
+}
+
+exports.newuser = function(req, res) {
+	res.json(req.body);
+}
+
+exports.updateuser = function(req, res) {
+	var user = {};
+	console.log(req.body);
+	user.name = req.body.name;
+	user.email = req.body.email;
+	res.json(user);
+}
+
+exports.photo = function(req, res) {
+	var user = {};
+	user.name = req.body.name;
+	user.email = req.body.email;
+	var image = req.files.photo;
+	console.log(req);
+	user.photo = image.name;
+	res.json(user);
+}
